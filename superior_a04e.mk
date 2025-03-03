d88d47190ed21bcf3e188c9d2370c63182ea28dc
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-03 07:56:52 +0530

    Update and rename orion_a04e.mk to superior_a04e.mk

--- superior_a04e.mk
+++ superior_a04e.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a04e/device.mk)
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs04.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 WITH_GMS := true
 PRODUCT_DEVICE := a04e
-PRODUCT_NAME := lineage_a04e
+PRODUCT_NAME := superior_a04e
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A04e
 PRODUCT_MANUFACTURER := samsung
