3b34590539729346b5d8be4fa13c8ea9bba8c9f1
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-17 17:09:20 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/alpha_a04e.mk
+    $(LOCAL_DIR)/derp_a04e.mk
 
 COMMON_LUNCH_CHOICES := \
-    alpha_a04e-user \
-    alpha_a04e-userdebug \
-    alpha_a04e-eng
+    derp_a04e-user \
+    derp_a04e-userdebug \
+    derp_a04e-eng
