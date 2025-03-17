c03a5004b8ec0159b220dc90228ed98e665827dc
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-17 17:11:29 +0530

    Update and rename alpha_a04e.mk to derp_a04e.mk

--- derp_a04e.mk
+++ derp_a04e.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a04e/device.mk)
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs04.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 WITH_GMS := true
 PRODUCT_DEVICE := a04e
-PRODUCT_NAME := aosp_a04e
+PRODUCT_NAME := derp_a04e
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A04e
 PRODUCT_MANUFACTURER := samsung
@@ -31,7 +31,6 @@ WITH_GMS := true
 TARGET_BUILD_PACKAGE := 3
 
 # Maintainer
-ALPHA_MAINTAINER := Smiley_9000
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
