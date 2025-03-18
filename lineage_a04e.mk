4d543b0fd42bdaa9670be19e08ab0a54a6e247aa
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-18 07:58:08 +0530

    Update and rename derp_a04e.mk to lineage_a04e.mk

--- lineage_a04e.mk
+++ lineage_a04e.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a04e/device.mk)
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs04.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 WITH_GMS := true
 PRODUCT_DEVICE := a04e
-PRODUCT_NAME := derp_a04e
+PRODUCT_NAME := lineage_a04e
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A04e
 PRODUCT_MANUFACTURER := samsung
@@ -30,7 +30,17 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 WITH_GMS := true
 TARGET_BUILD_PACKAGE := 3
 
-# Maintainer
+
+RISING_MAINTAINER="Smiley_9000"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="MediaTek P35" \
+    RisingMaintainer="Smiley_9000"
+
+RISING_MAINTAINER := Smiley_9000
+TARGET_ENABLE_BLUR := false
+
+TARGET_CORE_GMS := true
+TARGET_INCLUDE_GOOGLE_DIALER := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
